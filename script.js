e636d74b18b041ba76ee0006029b05874180ae4f
Darren Thompson <darren.thompson200@gmail.com>
2020-11-18 23:35:54 -0500

    Add ten airport graph

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function Node(id, label, x, y, size) {
 function createEdge(airportArr, id, i, j) {
     let src = airportArr[i].id;
     let target = airportArr[j].id;
-    console.log(src, target);
-    return { id: id, label: '$255', source: src, target: target };
+    let price = Math.floor(Math.random() * 201) + 200;
+    return { id: id, source: src, target: target, label: `$${price}` };
 }
 
 function getNodePrice(node) {
@@ -20,11 +20,24 @@ function getNodePrice(node) {
 
 
 // 10-airport system
-let s1 = new sigma('container');
+let s1 = new sigma({
+    renderer: {
+        container: document.getElementById('ten-node-container'),
+        type: 'canvas'
+    },
+    settings: {
+        edgeColor: 'default',
+        defaultEdgeColor: '#999',
+        sideMargin: 0.2,
+        defaultEdgeLabelColor: '#c00',
+        defaultEdgeLabelSize: 17,
+        font: 'Roboto'
+    }
+});
 let graph = s1.graph;
 
 let tenAirports = [];
-let tenAirportsAdj = new Array(10);
+let tenAirportsAdj = new Array(10); // Adjacency Matrix
 tenAirports.push(Node('n0', 'SEA', 0, 0, 3));
 tenAirports.push(Node('n1', 'SFO', 0, 2, 3));
 tenAirports.push(Node('n2', 'LAX', 0, 3, 3));
@@ -45,10 +58,25 @@ for (let i = 0; i < 10; i++) {
 }
 
 tenAirports.forEach(node => {
+    let option = document.createElement('option');
+    let textNode = document.createTextNode(`${node.label}`);
+    option.appendChild(textNode);
+    document.getElementById('ten-airport-select').appendChild(option);
     graph.addNode(node);
 });
 
+
+tenAirportsAdj[0][1] = 1;
+tenAirportsAdj[0][8] = 1;
+tenAirportsAdj[1][9] = 1;
+tenAirportsAdj[2][3] = 1;
 tenAirportsAdj[2][6] = 1;
+tenAirportsAdj[2][9] = 1;
+tenAirportsAdj[3][6] = 1;
+tenAirportsAdj[4][6] = 1;
+tenAirportsAdj[5][7] = 1;
+tenAirportsAdj[6][7] = 1;
+tenAirportsAdj[8][9] = 1;
 
 // Create edges with the adjacency matrix
 let edgeCount = 0;
@@ -60,12 +88,19 @@ for (let i = 0; i < 10; i++) {
         }
     }
 }
-
-
-s1.settings({
-    edgeColor: 'default',
-    defaultEdgeColor: '#999',
-    sideMargin: 0.2
-});
-
 s1.refresh();
+
+// Add the reverse of the edges
+// to the adjacency matrix after
+// rendering to avoid double lines
+tenAirportsAdj[1][0] = 1;
+tenAirportsAdj[8][0] = 1;
+tenAirportsAdj[9][1] = 1;
+tenAirportsAdj[3][2] = 1;
+tenAirportsAdj[6][2] = 1;
+tenAirportsAdj[9][2] = 1;
+tenAirportsAdj[6][3] = 1;
+tenAirportsAdj[6][4] = 1;
+tenAirportsAdj[7][5] = 1;
+tenAirportsAdj[7][6] = 1;
+tenAirportsAdj[9][8] = 1;
